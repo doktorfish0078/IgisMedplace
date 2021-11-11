6c36b61820c928ded47e1954149740f05b8b50fa
DoktorFish0078 <DoktorFish0078@mail.ru>
2021-11-11 04:04:16 +0400

    add func reading/writing data in/out json-file
    and adaption code

--- parsing_site/parsing.py
+++ parsing_site/parsing.py
@@ -1,24 +1,44 @@
+import json
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://igis.ru/online'
-html_home_page = requests.get(URL).text
 
-soup = BeautifulSoup(html_home_page, features='html.parser')
+def parse():
+    URL = 'https://igis.ru/online'
+    html_home_page = requests.get(URL).text
 
-towns = soup.find_all('div', {'class': 'row-div row-div-sm-2 row-div-md-1 row-div-bg-1'})
+    soup = BeautifulSoup(html_home_page, features='html.parser')
 
-towns_link = []
+    towns = soup.find_all('div', {'class': 'row-div row-div-sm-2 row-div-md-1 row-div-bg-1'})
 
-for i in range(0, 28):
-    towns_link.append(towns[i].find('a')['href'])
+    data_towns = {}
 
-hospitals = []
+    for i in range(0, 28):
+        elem_town = towns[i].find('a')
+        data_towns[i] = {'town_name': elem_town.text, 'town_href': URL + elem_town['href'], 'hospitals': []}
 
-for town_link in towns_link:
-    html_town_page = requests.get(URL + town_link).text
-    soup = BeautifulSoup(html_town_page, features='html.parser')
-    h3 = soup.find_all('h3')
-    h3 = h3[3:]
-    for h in h3:
-        hospitals.append(h.find('a')['href'])
+    for key_town in data_towns:
+        html_town_page = requests.get(data_towns[key_town]['town_href']).text
+        soup = BeautifulSoup(html_town_page, features='html.parser')
+        h3 = soup.find_all('h3')
+        h3 = h3[3:]
+        for h in h3:
+            elem_hospital = h.find('a')
+            data_towns[key_town]['hospitals'].append(
+                {'name_hospital': elem_hospital.text, 'href_hospital': URL + elem_hospital['href']})
+    write_file(data_towns)
+
+
+def write_file(data):
+    with open('dataa.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+def read_file():
+    with open('dataa.txt') as json_file:
+        data = json.load(json_file)
+        for key in data:
+            print(data[key])
+
+
+read_file()
